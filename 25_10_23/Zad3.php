0ebee282058a69e7046f50e89c79ba0445fc198c
GeorgeWeak <paplo.picaso@o2.pl>
2023-10-26 14:59:09 +0200

    1

---
+++ 25_10_23/Zad3.php
@@ -0,0 +1,16 @@
+<?php
+$cookie_name = "username";
+$cookie_value ="Pawel paw\5C_Prac_Aplik-Web_2023_2024/25_10_23/Zad1.php";
+setcookie($cookie_name,$cookie_value);
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zad3</title>
+</head>
+<body>
+    
+</body>
+</html>
