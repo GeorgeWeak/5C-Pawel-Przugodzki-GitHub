06eb6f548b7c9f36e8dd4d46c3d4b93532fbd6cb
GeorgeWeak <paplo.picaso@o2.pl>
2023-11-02 15:12:37 +0100

    1

---
+++ 24_10_23/airport.php
@@ -0,0 +1,76 @@
+<?php
+$cookiename = "IsVisited";
+$cookievalue = "Dzień dobry!";
+if(!isset($_COOKIE[$cookiename])){
+    setcookie($cookiename,$cookievalue,time()+3600);
+}
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Odloty samolotów</title>
+    <link rel="stylesheet" href="styl6.css">
+</head>
+<body>
+    <header>
+        <section id="bannerLeft">
+            <h2>Odloty z lotniska</h2>
+        </section>
+
+    <section id="bannerRight">
+        <img src="zad6.png" alt="logotyp">
+    </section>
+</header>
+<main>
+    <h4>tabela odlotow</h4>
+    <table>
+        <tr>
+            <th>lp.</th>
+            <th>numer rejsu</th>
+            <th>czas</th>
+            <th>kierunek</th>
+            <th>status</th>
+        </tr>
+        <?php
+        $host = 'localhost';
+        $user = 'root';
+        $pass = '';
+        $db =  'egzamin';
+
+        $connect = mysqli_connect($host,$user,$pass,$db);
+        
+        $query = 'SELECT id, nr_rejsu, czas, kierunek, status_lotu FROM `odloty` ORDER BY czas DESC;';
+
+        $result  = mysqli_query($connect,$query);
+
+        while($row = mysqli_fetch_assoc($result)){
+            echo "<tr>";
+            echo "<td>". $row['id']."</td>";
+            echo "<td>". $row['nr_rejsu']."</td>";
+            echo "<td>". $row['czas']."</td>";
+            echo "<td>". $row['czas']."</td>";
+            echo "<td>". $row['status_lotu']."</td>";
+        }
+        mysqli_close($connect);
+        ?>
+</table>
+</main>
+<footer>
+    <section id="leftFooter">
+        <a href="kw1.png" target="_blank">Pobierz obraz</a>    
+    </section>
+    <section id="midFooter">
+        <?php
+           if(!isset($_COOKIE[$cookiename])){
+            echo "<p><i> Dzień dobry! Sprawdz regulamin naszej strony</i></p>";
+           }else echo "<p><b>Miło nam ze nas znowu odwiedziłes</p></b>"
+        ?>
+        </section>
+        <section id="rightFooter">
+            Autor Paweł Przygodzki
+        </section>
+    </footer>
+</body>
+</html>
