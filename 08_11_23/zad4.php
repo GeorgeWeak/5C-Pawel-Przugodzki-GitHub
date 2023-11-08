ed444df00d122b736dbd24c21568ac3a8b7bfb8d
GeorgeWeak <paplo.picaso@o2.pl>
2023-11-08 13:48:47 +0100

    zad4

---
+++ 08_11_23/zad4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zad4</title>
+</head>
+<body>
+    <?php
+    $a = 34;
+    $b = 27;
+
+    if($a > $b){
+        echo "<p>zmienna 'a' jest wieksza od zmiennej 'b'</p> ";
+    }elseif($a==$b){
+        echo "<p>zmienne 'a' i 'b' sa takie same</p>";
+    }else{
+        echo "<p>zmienna 'a'jest mniejsza od 'b'</p> ";
+    }
+    ?>
+    
+</body>
+</html>
