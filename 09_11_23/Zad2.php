e3384e9602012639ca92a81ac9acbd399b906752
GeorgeWeak <paplo.picaso@o2.pl>
2023-11-09 14:08:25 +0100

    zad1-3

---
+++ 09_11_23/Zad2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zad2</title>
+</head>
+<body>
+    <?php
+    $liczba = 100;
+
+    for ($a = 0; $a < 10; $a++){
+        echo $liczba . ' ';
+        $liczba--;
+    }
+    ?>
+    
+</body>
+</html>
---
+++ 09_11_23/Zad3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zad3</title>
+</head>
+<body>
+    <?php
+    $liczba = 100;
+
+    for ($a = 0; $a < 5; $a++){
+        echo $liczba . ' ';
+        $liczba += 5;
+    }
+    ?>
+
+    
+</body>
+</html>
