9469a71e9c400d321f9ded8dd60138c625c085d3
GeorgeWeak <paplo.picaso@o2.pl>
2023-11-15 15:04:16 +0100

    Zad3

---
+++ 15_11_23/Zad3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zad3</title>
+</head>
+<body>
+    <?php
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "ok";
+
+    $conn = mysqli_connect($host, $username, $password, $database);
+
+    if (!$conn) {
+        die("błąd połaczenia" . mysqli_connect_error());
+    }
+
+    mysqli_connect($conn);
+    
+</body>
+</html>m
